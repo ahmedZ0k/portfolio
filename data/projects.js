5425eb92a9725cd9ce5303d8c24c4c558d4a38b9
Ahmed Zakaria <ahmedzakria102@gmail.com>
2024-02-29 21:30:24 +0200

    fix images path

--- data/projects.js
+++ data/projects.js
@@ -1,3 +1,7 @@
+import Ecomerce from '../src/assets/e-commerce-screenshot.png';
+import Pizza from '../src/assets/react-pizza-screenshot.png';
+import Watch from '../src/assets/watch-movie-screenshot.png';
+
 const projects = [
   {
     id: 1,
@@ -5,9 +9,9 @@ const projects = [
     description:
       'This is an E-commerce API that I developed. It includes user authentication and authorization features. It also provides admin control and all the standard features of an e-commerce website.',
     skills: ['Node JS', 'Express JS', 'MongoDB'],
-    screenShot: '../src/assets/e-commerce-screenshot.png',
     github: 'https://github.com/zekooo0/e-commerce-backend',
     liveDemo: '',
+    screenShot: Ecomerce,
   },
   {
     id: 2,
@@ -15,9 +19,9 @@ const projects = [
     description:
       'React Pizza is a web application that allows users to browse through a pizza menu, add their chosen pizzas to a cart, and confirm their order. It provides a seamless user experience for ordering pizzas online.',
     skills: ['React JS', 'Redux Toolkit', 'Tailwind CSS'],
-    screenShot: '../src/assets/react-pizza-screenshot.png',
     github: 'https://github.com/zekooo0/React-Pizza',
     liveDemo: 'https://react-pizza-lyart-nine.vercel.app/',
+    screenShot: Pizza,
   },
   {
     id: 3,
@@ -25,9 +29,9 @@ const projects = [
     description:
       'Watched List is a web application that allows users to search for any film, add it to their watched list, and rate the film. It provides a user-friendly interface for tracking and rating films.',
     skills: ['React JS'],
-    screenShot: '../src/assets/watch-movie-screenshot.png',
     github: 'https://github.com/zekooo0/Watched-List',
     liveDemo: 'https://watched-list.vercel.app/',
+    screenShot: Watch,
   },
 ];
 
